4ac1292494ad085748eae3532a606fbd3fa1dd55
Gabriel Rzecznik <rzecznike@gmail.com>
2023-06-19 16:23:51 -0300

    Mejoras en el orden de imports y elinimación de código en desuso del navegador

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,23 +2,23 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule } from '@angular/common/http';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import {MatChipsModule} from '@angular/material/chips';
-import {MatBadgeModule} from '@angular/material/badge';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 //Components
 import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
 import { ProductsComponent } from './components/products/products.component';
 import { SubcategoriesComponent } from './components/subcategories/subcategories.component';
-import { NavComponent } from './components/nav/nav.component';
+import { SubcategoryBannerComponent } from './components/subcategory-banner/subcategory-banner.component';
 
 //Material
 import { MatIconModule } from '@angular/material/icon';
 import { MatButtonModule } from '@angular/material/button';
-import { MatToolbarModule } from '@angular/material/toolbar';//Nav
+import { MatToolbarModule } from '@angular/material/toolbar';
 import { MatCardModule } from '@angular/material/card';
 import { MatExpansionModule } from '@angular/material/expansion';
-import { SubcategoryBannerComponent } from './components/subcategory-banner/subcategory-banner.component';
+import { MatChipsModule } from '@angular/material/chips';
+import { MatBadgeModule } from '@angular/material/badge';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
 
 @NgModule({
   declarations: [
